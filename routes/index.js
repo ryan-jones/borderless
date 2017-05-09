713c793fd59d56a11773ca6fcd9593fdc5908c55
Ryan Jones <ryanjones@Ryans-MacBook.local>
2017-05-09 09:46:40 +0200

    updated files on new computer

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
+const bcrypt   = require("bcrypt");
 const Company = require('../models/company');
+const User     = require("../models/user");
+const passport = require("../helpers/passport");
+const bcryptSalt = 10;
+var auth    = require('../helpers/auth');
+
 var router = express.Router();
+
 
 
 
@@ -52,12 +59,89 @@ router.route('/explore')
 
 
 
-router.route('/new')
-  .get((req, res, next) => {
-        res.render('companies/new');
-      }
-    );
 
+
+/* GET users listing. */
+router.get('/signup', function(req, res, next) {
+  res.render('auth/signup', { "message": req.flash("error") });
+});
+
+router.post("/signup", (req, res, next) => {
+  var name = req.body.name;
+  var username = req.body.username;
+  var password = req.body.password;
+  var nationality = req.body.nationality;
+  var role = req.body.usertype;
+
+  if (username === "" || password === "") {
+  	req.flash('error', 'Indicate username and password' );
+    res.render("auth/signup", { "message": req.flash("error") });
+    return;
+  }
+
+  User.findOne({ username }, "name", (err, user) => {
+    if (user !== null) {
+    	req.flash('error', 'The username already exists' );
+      res.render("auth/signup", { message: req.flash("error") });
+      return;
+    }
+
+    var salt     = bcrypt.genSaltSync(bcryptSalt);
+    var hashPass = bcrypt.hashSync(password, salt);
+
+    var newUser = User({
+      name,
+      username,
+      password: hashPass,
+      nationality,
+      role
+    });
+
+    newUser.save((err) => {
+      if (err) {
+      	req.flash('error', 'The username already exists' );
+        res.render("auth/signup", { message: req.flash('error') });
+      } else {
+        passport.authenticate("local")(req, res, function () {
+           res.redirect('/login');
+        });
+      }
+    });
+  });
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login", { "message": req.flash("error") });
+});
+
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/users/index/",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+
+router.get('/users/index/', auth.checkLoggedIn('You must be login', '/login'), (req, res, next) => {
+  if(req.user.role === "USER") {
+    res.render('users/index', { user: JSON.stringify(req.user) });
+  } else {
+    res.render('companies/index', { user: JSON.stringify(req.user) });
+}});
+
+router.get('/new', auth.checkLoggedIn('You must be login', '/login'), auth.checkCredentials('COMPANY'), (req, res, next) => {
+  res.render('companies/new', { user: JSON.stringify(req.user) });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  delete res.locals.currentUser;
+  delete req.session.passport;
+  // delete currentUser and passport properties 
+  // becasuse when we calling req.logout() is leaving an empty object inside both properties.
+  res.redirect('/');
+});
 
 
 router.get('/test', (req, res, next) =>{
