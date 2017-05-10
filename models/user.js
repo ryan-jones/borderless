3b7a65d87b577924a45791d421e4553c6c0bfadf
Juan C. Rodriguez <rodriguez.juan.c@gmail.com>
2017-05-10 14:54:21 +0200

    created link to signup from landing page, updated signup form order

--- models/user.js
+++ models/user.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: String,
-  username: String,
-  password: String,
-  nationality: String,
   role: {
     	type: String,
     	enum : ['USER', 'COMPANY', 'ADMIN'],
     	default : 'USER'
-  	}
+  	},
+  name: String,
+  username: String,
+  password: String,
+  nationality: String
 	}, {
   	timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 	}
